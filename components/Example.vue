d983bf55752ecbf7e794f8482355a1818b647774
Anudeep <anudeep@ortharize.com>
2024-03-10 12:08:32 +0530

    Initial commit

---
+++ components/Example.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button v-if="$auth.loggedIn" @click="$auth.logout()">Sign Off</button>
+    <button v-else @click="$auth.loginWith('auth0')">Sign In</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Example",
+  mounted() {
+    console.log("dssdsdsdsdsdsd", this.$auth);
+  },
+};
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,47 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "auth0-test",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  css: ["normalize.css", "@/assets/main.css"],
+  /*
+   ** Modules
+   */
+  modules: [
+    // axios is required by @nuxtjs/auth
+    "@nuxtjs/axios",
+    // https://auth.nuxtjs.org
+    "@nuxtjs/auth",
+  ],
+  auth: {
+    redirect: {
+      login: "/", // redirect user when not connected
+      callback: "/auth/signed-in",
+    },
+    strategies: {
+      local: false,
+      auth0: {
+        domain: process.env.AUTH0_DOMAIN,
+        client_id: process.env.AUTH0_CLIENT_ID,
+      },
+    },
+  },
+  build: {
+    // For stormkit.io
+    publicPath: process.env.PUBLIC_PATH,
+  },
+};
